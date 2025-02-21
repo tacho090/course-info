f778a369d83290e4f25de9e252c426c1b3c4d3f7
Pablo Mora <pablo.mora@experian.com>
2025-02-20 23:30:10 -0600

    update slf4j maven dependencies

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,22 +1,26 @@
 package com.pluralsight.courseinfo.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
     public static void main(String... args) {
-        System.out.println("hello world");
+        LOG.info("CourseRetriever starting");
         if (args.length == 0) {
-            System.out.println("Please provide an author name as first arg");
+            LOG.warn("Please provide an author name as first arg");
             return;
         }
         try {
             retrieveCourses(args[0]);
         } catch (Exception e) {
-            System.out.println("Unexpected error");
-            e.printStackTrace();
+            LOG.error("Unexpected error");
         }
     }
 
     private static void retrieveCourses(String authorId) {
-        System.out.println("Retrieving courses for author " + authorId);
+        LOG.info("Retrieving courses for author " + authorId);
     }
 }
