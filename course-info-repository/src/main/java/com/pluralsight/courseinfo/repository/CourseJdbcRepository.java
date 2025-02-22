f526c468036c0ceddf0f62292cde43bfdc408bfc
Pablo Mora <pablo.mora@experian.com>
2025-02-22 09:50:06 -0600

    Add Exception for sql statement

--- course-info-repository/src/main/java/com/pluralsight/courseinfo/repository/CourseJdbcRepository.java
+++ course-info-repository/src/main/java/com/pluralsight/courseinfo/repository/CourseJdbcRepository.java
@@ -4,11 +4,19 @@ import com.pluralsight.courseinfo.domain.Course;
 import org.h2.jdbcx.JdbcDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class CourseJdbcRepository implements CourseRepository {
     private static final String H2_DATABASE_URL =
             "jdbc:h2:file:%s;AUTO_SERVER=TRUE;INIT=RUNSCRIPT FROM './db_init.sql'";
+
+    private static final String INSERT_COURSE = """
+            MERGE INTO Courses (id, name, length, url)
+            VALUES (?, ?, ?, ?)
+            """;
 
     private final DataSource dataSource;
 
@@ -20,6 +28,17 @@ public class CourseJdbcRepository implements CourseRepository {
 
     @Override
     public void saveCourse(Course course) {
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(INSERT_COURSE);
+            statement.setString(1, course.id());
+            statement.setString(2, course.name());
+            statement.setLong(3, course.length());
+            statement.setString(4, course.url());
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RepositoryException("Failed to save " + course, e);
+        }
 
     }
 
